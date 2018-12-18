aa8095c694dc151e8be20de36ab833cc6a587882
Heena110297 <44188323+Heena110297@users.noreply.github.com>
2018-12-18 11:56:37 +0530

    outputting content

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit {
+ newPost = 'No Content';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddPost() {
+    this.newPost = 'The user\'s post';
+  }
+}
