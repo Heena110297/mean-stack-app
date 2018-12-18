cf1c5e7b2b8cd31cd202d7d79ceedd556493cb91
Heena110297 <44188323+Heena110297@users.noreply.github.com>
2018-12-18 18:03:06 +0530

    Added form

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit , Output, EventEmitter} from '@angular/core';
 import {Post} from '../post.model';
+import {NgForm} from '@angular/forms';
+import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
@@ -16,11 +18,14 @@ export class PostCreateComponent implements OnInit {
   ngOnInit() {
   }
 
-  onAddPost() {
-const post: Post = {
-  title: this.enteredTitle,
-  content: this.enteredContent
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+  const post: Post = {
+  title: form.value.title,
+  content: form.value.content
 };
 this.postCreated.emit(post);
-  }
+}
 }
