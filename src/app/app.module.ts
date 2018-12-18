a5e566cab253a28ab956f364241c4f40b05bcca4
Heena110297 <44188323+Heena110297@users.noreply.github.com>
2018-12-18 13:17:50 +0530

    Using Angular-Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule, MatCardModule,MatButtonModule} from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,7 +14,11 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
