89c0fa29f0b2ecf902710bac3e2b44c9c455d62b
Heena110297 <44188323+Heena110297@users.noreply.github.com>
2018-12-19 11:44:42 +0530

    Used Rxjs

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,21 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import {Post} from '../post.model';
 import { PostsService } from '../posts.service';
+import {Subscription} from 'rxjs';
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
    posts: Post[] = [
   //   {title: 'First Post', content: 'This is the First Post'},
   //   {title: 'Second Post', content: 'This is the Second Post'},
   //   {title: 'Third Post', content: 'This is the Third Post'}
    ];
+   private postsSub: Subscription;
   constructor(public postsService: PostsService) { }
 
   ngOnInit() {
     this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+    .subscribe((posts: Post[]) => {
+      this.posts = posts ;
+    });
+    // function which get sexecuted whenever a new value is recieved
+    // error is emitted
+    // observable is completed no more data expected
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
   }
 
 }
