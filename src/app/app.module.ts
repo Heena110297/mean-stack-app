1cedb2e8366f966339b3eed32efa53d99f79ca44
Heena110297 <44188323+Heena110297@users.noreply.github.com>
2019-01-22 12:53:11 +0530

    Converted the form to a reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatInputModule, MatToolbarModule, MatExpansionModule, MatCardModule, MatButtonModule, MatProgressSpinnerModule} from '@angular/material';
+import {MatInputModule,
+        MatToolbarModule,
+        MatExpansionModule,
+        MatCardModule,
+        MatButtonModule,
+        MatProgressSpinnerModule} from '@angular/material';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { PostsService } from './posts/posts.service';
@@ -19,7 +24,7 @@ import {HttpClientModule} from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatInputModule,
